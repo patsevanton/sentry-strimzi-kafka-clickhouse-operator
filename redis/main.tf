df7baee2b282019957fdf48120fee6df6da2543f
Anton Patsev <patsev.anton@gmail.com>
2024-06-27 11:14:36 +0600

    fix sentry-redis

--- redis/main.tf
+++ redis/main.tf
@@ -2,7 +2,7 @@ module "redis" {
   source  = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-redis.git?ref=init"
 
   folder_id   = "xxxx"
-  name        = "test-redis"
+  name        = "sentry-redis"
   subnet_id   = "xxxxx"
   network_id  = "xxxx"
   password    = "xxxx"
