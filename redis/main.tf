9edd7f11bc57ba0607c9512a685c9c31b6aa1f69
Anton Patsev <patsev.anton@gmail.com>
2024-06-27 16:44:14 +0600

    change redis to apatsev

--- redis/main.tf
+++ redis/main.tf
@@ -2,7 +2,7 @@ module "redis" {
   source  = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-redis.git?ref=init"
 
   folder_id   = "xxxx"
-  name        = "sentry-redis"
+  name        = "apatsev-redis"
   subnet_id   = "xxxxx"
   network_id  = "xxxx"
   password    = "xxxx"
