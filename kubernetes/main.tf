8a83a32623a2095ffbe004f6c7a9d4176883bc8e
Anton Patsev <patsev.anton@gmail.com>
2024-06-28 16:41:05 +0600

    fix node_groups

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -39,7 +39,7 @@ module "kube" {
   ]
 
   node_groups = {
-    "fixed_scale" = {
+    "fixed-scale" = {
       nat    = true
       cores  = 2
       memory = 4
@@ -48,10 +48,10 @@ module "kube" {
       }
     }
 
-    "auto_scale" = {
+    "auto-scale" = {
       nat    = true
-      memory = 2
-      cores  = 8
+      cores  = 2
+      memory = 8
       auto_scale = {
         min     = 1
         max     = 5
