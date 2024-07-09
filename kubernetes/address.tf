1387f31b39f8f3ef84b0fb33b06393db08215362
Anton Patsev <patsev.anton@gmail.com>
2024-07-09 19:09:27 +0600

    format tf

--- kubernetes/address.tf
+++ kubernetes/address.tf
@@ -2,6 +2,6 @@ module "address" {
   source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-address.git"
 
   ip_address_name = "apatsev-pip"
-  folder_id = "xxxx"
-  zone = "ru-central1-a"
+  folder_id       = "xxxx"
+  zone            = "ru-central1-a"
 }
