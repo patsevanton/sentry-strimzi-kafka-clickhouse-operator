ef1c3ac1433a0ed402bd14bb27bfaab08b632df2
Anton Patsev <patsev.anton@gmail.com>
2024-07-11 16:31:59 +0600

    fix fqdn_redis

--- redis/outputs.tf
+++ redis/outputs.tf
@@ -1,8 +1,12 @@
-output "fqdn_redis" {
+output "fqdn" {
   value = module.redis.fqdn
 }
 
 output "password" {
   value     = module.redis.password
   sensitive = true
+}
+
+output "fqdn_redis" {
+  value = module.redis.fqdn_redis
 }
