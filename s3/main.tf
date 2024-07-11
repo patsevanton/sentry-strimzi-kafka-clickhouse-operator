266c361831b7a8793ea73ea0f7cfcb9f4e41a24e
Anton Patsev <patsev.anton@gmail.com>
2024-07-11 11:47:07 +0600

    change s3 module to terraform-yacloud-modules

--- s3/main.tf
+++ s3/main.tf
@@ -1,6 +1,6 @@
 
 module "s3" {
-  source = "git::https://github.com/terraform-yc-modules/terraform-yc-s3.git"
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-storage-bucket.git"
 
   bucket_name = "sentry-bucket-apatsev-dev"
   folder_id   = "xxxx"
